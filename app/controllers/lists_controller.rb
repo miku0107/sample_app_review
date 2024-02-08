dd19cb4845cb03ea58a42f7bfee2f862bed51bb5
miku0107 <138674231+miku0107@users.noreply.github.com>
2024-02-08 07:31:46 +0000

    make list_create_action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,12 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end  
 
   def index
   end
@@ -11,4 +17,11 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end  
+  
 end
